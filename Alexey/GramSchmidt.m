88216a9a8aa8c0a35974b7fc04228c1a6f75edd8
Alexey Petrov <petrov@mail.ioffe.ru>
2013-10-18 05:15:30 +0400

    Ортогонализационная процедура Грама-Шмидта

---
+++ Alexey/GramSchmidt.m
@@ -0,0 +1,16 @@
+(* ::Package:: *)
+
+BeginPackage["Alexey`GramSchmidt`"]
+
+
+ProjectionO::usage="ProjectionO[v1,v2] - it is projection vector v1 on v2";
+GramSchmidt::usage="Procedure of the orthogonalisation for matrix of eighenvalues";
+
+
+
+ProjectionO[v1_,v2_]:=(v1.v2*v2)/v2.v2;
+MultipleProjection[v1_,vecs_]:=Plus@@(ProjectionO[v1,#1]&)/@vecs;
+GramSchmidt[mat_]:=Fold[Join[#1,{#2-MultipleProjection[#2,#1]}]&,{},mat];
+
+
+EndPackage[]
